b2453b264bf43c61290f762065b62e9eafbb39e7
deutschluz <deutschluz@gmail.com>
2021-01-22 09:57:23 -0800

    corrected qsort implementation

--- Ch7/qsort.lua
+++ Ch7/qsort.lua
@@ -9,20 +9,20 @@ end
 function partition(t,left,right)
   local x=t[right]
   local i=left-1
-  for j=p,right-1 do
+  for j=left,right-1 do
     if t[j]<=x then
       i=i+1
       swap(t,i,j)
     end
   end
-  swap(t,i+1,r)
+  swap(t,i+1,right)
   return i+1
 end
 
 function qsort(t,left,right)
 --  dbg()
   if left < right then
-    local q=partition(t,left,pivot)
+    local q=partition(t,left,right)
     qsort(t,left,q-1)
     qsort(t,q+1,right)
   end
@@ -38,7 +38,7 @@ end
 function main()
   local l={10,-1,3,6,8,0,2,-5}
   printTab(l)
-  qsort(l,1,#l-1)
+  qsort(l,1,#l)
   printTab(l)
 end
 
